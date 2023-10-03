e74f3e6870b1ef7c3119fa0f684a239ecc98bd7c
hpcfung <91354621+hpcfung@users.noreply.github.com>
2023-10-03 02:34:43 -0400

    Add files via upload

---
+++ 2023/exam_typesetting/MCQ.js
@@ -0,0 +1,89 @@
+// setTimeout(function() {
+//     let input_path = prompt("Enter absolute path to .jl (\"Copy as path\")", "\"C:\\Python_projects\\2023 USRA\\april_28_plot_test.jl\"");
+//     REPL_code = "include("+input_path.replaceAll("\\","\\\\")+")";
+//     code.textContent = REPL_code;
+//     navigator.clipboard.writeText(REPL_code);
+    
+// }, (1));
+
+// need preprocessing: page skip, Q gap
+// also when merging different sources, need gap btw Qs
+
+function lowerOpacity(buttonElement,i) {
+    // console.log(i)
+    buttonElement.style.opacity = 1-i/200;
+    if (i < 100) {
+        const myTimeout = setTimeout(lowerOpacity.bind(null, buttonElement,i+1), 10);
+    } else {
+        buttonElement.textContent = "Add raw text";
+        buttonElement.style.opacity = 1;
+    }
+}
+
+function getCompiledText(raw_text) {
+  // remove first and last empty line
+  processed_text = raw_text.replace(/^\s*\n/, '')
+  processed_text = processed_text.replace(/\n\s*$/, '')
+  // multiline break: gap should be 1
+  processed_text = processed_text.replaceAll(/\n\s*\n\s*\n/g, '\n\n')
+  
+  // Q:, X detect whitespace after Q number; removes all preceding whitespace, \s without \n
+  processed_text = processed_text.replaceAll(/^[\f\r\t\v\u0020\u00a0\u1680\u2000-\u200a\u2028\u2029\u202f\u205f\u3000\ufeff]*[0-9]+[).]/mg, '_Q:')
+  // make sure there is exactly one whitespace after Q number
+  processed_text = processed_text.replaceAll(/_Q:[\f\r\t\v\u0020\u00a0\u1680\u2000-\u200a\u2028\u2029\u202f\u205f\u3000\ufeff]*/g, '_Q: ')
+  
+  // A:
+  processed_text = processed_text.replaceAll(/^[\f\r\t\v\u0020\u00a0\u1680\u2000-\u200a\u2028\u2029\u202f\u205f\u3000\ufeff]*[A-Fa-f][.)]?[ ]/mg, '_A:')
+  processed_text = processed_text.replaceAll(/_A:[\f\r\t\v\u0020\u00a0\u1680\u2000-\u200a\u2028\u2029\u202f\u205f\u3000\ufeff]*/g, '_A: ')
+  return processed_text
+}
+
+function repr(str) {
+    var result = '';
+    for (var i = 0; i < str.length; i++) {
+      var char = str.charAt(i);
+      switch (char) {
+        case '\n':
+          result += '\\n';
+          break;
+        case '\r':
+          result += '\\r';
+          break;
+        case '\t':
+          result += '\\t';
+          break;
+        case ' ':
+          result += '\\s';
+          break;
+        // Add more cases for other special characters as needed
+        default:
+          result += char;
+      }
+    }
+    return result;
+  }
+
+  
+function addRawText() {
+  // Get the text area
+  var raw_text = document.getElementById("rawText").value;
+  console.log("Raw text:")
+  console.log(raw_text)
+  console.log(repr(raw_text))
+  console.log("Processed text:")
+  console.log(repr(getCompiledText(raw_text)))
+  console.log(typeof raw_text)
+  if (document.getElementById("compiledText").value === "") {
+    document.getElementById("compiledText").value = getCompiledText(raw_text)
+  } else {
+    document.getElementById("compiledText").value += "\n\n" + getCompiledText(raw_text)
+  }
+  // actually, Qs from each iteration should be processed separately to avoid cross talk
+  // use a simple function to enforce 1 space in between
+  
+  // Refresh button and rawText textarea
+  buttonElement = document.getElementById("add_button")
+  buttonElement.textContent = "Added!";
+  const myTimeout = setTimeout(lowerOpacity.bind(null, buttonElement,1), 10);
+  document.getElementById("rawText").value = ""
+}
