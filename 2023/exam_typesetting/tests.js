b845d5182cbd7fc0744df1d3cbc9fa0890712e07
hpcfung <91354621+hpcfung@users.noreply.github.com>
2023-10-17 08:06:36 -0400

    Update tests.js

--- 2023/exam_typesetting/tests.js
+++ 2023/exam_typesetting/tests.js
@@ -1,5 +1,21 @@
+function test1() {  
+  const test_str = '_Q: This is a question\n_A: This is an answer\n_Q: This is another question';
+  const array = [...test_str.matchAll(/(?<=_Q: ).*$/gm)];
+  const array = [...test_str.matchAll(/_[APQ].*$/gm)];
+  console.log(array);
+}
 
-const test_str = '_Q: This is a question\n_A: This is an answer\n_Q: This is another question';
-const array = [...test_str.matchAll(/(?<=_Q: ).*$/gm)];
+function test2() {
+  const test_str = '_Q: This is a question\n_A: This is an answer\n_Q: This is another question';
+
 const array = [...test_str.matchAll(/_[APQ].*$/gm)];
-console.log(array);
+extracted_text = array.map(arr => arr[0]);
+console.log(extracted_text);
+
+for (text_str of extracted_text) {
+    console.log('');
+    console.log(text_str);
+    console.log(text_str[1]);
+    console.log(text_str.slice(4));
+}
+}
