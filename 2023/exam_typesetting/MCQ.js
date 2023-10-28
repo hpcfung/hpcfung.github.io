52187d50b2c3b95d53d2aa286897794325256874
hpcfung <hpcfung@uwaterloo.ca>
2023-10-28 00:17:18 -0400

    Tab quick fix

--- 2023/exam_typesetting/MCQ.js
+++ 2023/exam_typesetting/MCQ.js
@@ -10,17 +10,20 @@ function getCompiledText(raw_text) {
   // [\f\r\t\v\u0020\u00a0\u1680\u2000-\u200a\u2028\u2029\u202f\u205f\u3000\ufeff]
   // disjunction |: matches left condition first?
   processed_text = processed_text.replaceAll(/^\s*[0-9]+(.\)|[.) ])/mg, '_Q:')
+  // no escaping required in a char class: .)
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Regular_expressions/Character_escape#:~:text=they%20require%20escaping-,unless,-in%20a%20character
   
   // A:
-  processed_text = processed_text.replaceAll(/^\s*[A-Fa-f](.\)|[.)])?[ ]/mg, '_A:')
-  processed_text = processed_text.replaceAll(/_A:[\f\r\t\v\u0020\u00a0\u1680\u2000-\u200a\u2028\u2029\u202f\u205f\u3000\ufeff]*/g, '_A: ')
+  // maybe change to sth like (?![\n])\s, nested groups tho?
+  processed_text = processed_text.replaceAll(/^\s*[A-Fa-f](.\)|[.)]|[ \t])/mg, '_A:')
+  processed_text = processed_text.replaceAll(/_A:[ \f\r\t\v\u0020\u00a0\u1680\u2000-\u200a\u2028\u2029\u202f\u205f\u3000\ufeff]*/g, '_A: ')
 
   // P: disjunction order matters
-  processed_text = processed_text.replaceAll(/^\s*(iii|ii|iv|v|viii|vii|vi|ix|x|i)(.\)|[.)])?[ ]/mgi, '_P:')
-  processed_text = processed_text.replaceAll(/_P:[\f\r\t\v\u0020\u00a0\u1680\u2000-\u200a\u2028\u2029\u202f\u205f\u3000\ufeff]*/g, '_P: ')
+  processed_text = processed_text.replaceAll(/^\s*(iii|ii|iv|v|viii|vii|vi|ix|x|i)(.\)|[.)])?[ \t]/mgi, '_P:')
+  processed_text = processed_text.replaceAll(/_P:[ \f\r\t\v\u0020\u00a0\u1680\u2000-\u200a\u2028\u2029\u202f\u205f\u3000\ufeff]*/g, '_P: ')
 
   // make sure there is exactly one newline before and one whitespace after Q number
-  processed_text = processed_text.replaceAll(/_Q:[\f\r\t\v\u0020\u00a0\u1680\u2000-\u200a\u2028\u2029\u202f\u205f\u3000\ufeff]*/g, '\n_Q: ')
+  processed_text = processed_text.replaceAll(/_Q:[ \f\r\t\v\u0020\u00a0\u1680\u2000-\u200a\u2028\u2029\u202f\u205f\u3000\ufeff]*/g, '\n_Q: ')
   
   // remove non Q, A, P lines
   const matches = [...processed_text.matchAll(/_[APQ].*$/gm)];
